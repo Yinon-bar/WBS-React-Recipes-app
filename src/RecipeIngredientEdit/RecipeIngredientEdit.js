15c0551a2749658b14241196da4605f4f0e9894e
Yinon bar <yinonbar1988@gmail.com>
2022-11-29 14:30:59 +0200

    context working

---
+++ src/RecipeIngredientEdit/RecipeIngredientEdit.js
@@ -0,0 +1,13 @@
+import "./RecipeIngredientEdit.css";
+
+function RecipeIngredientEdit() {
+  return (
+    <>
+      <input type="text" />
+      <input type="text" />
+      <button></button>
+    </>
+  );
+}
+
+export default RecipeIngredientEdit;
