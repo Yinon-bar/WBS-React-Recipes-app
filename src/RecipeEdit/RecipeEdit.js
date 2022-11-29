5eb5316b0f89608ad0685f73e34fba28517294f0
Yinon bar <yinonbar1988@gmail.com>
2022-11-29 13:44:01 +0200

    context working

--- src/RecipeEdit/RecipeEdit.js
+++ src/RecipeEdit/RecipeEdit.js
@@ -4,10 +4,35 @@ import { FaTimes } from "react-icons/fa";
 function RecipeEdit() {
   return (
     <div className="RecipeEdit">
-      <button className="btn danger">
-        <FaTimes />
-        <form className="Form"></form>
-      </button>
+      <div className="Headings">
+        <h1>Edit Recipe</h1>
+        <button className="btn danger">
+          <FaTimes />
+        </button>
+      </div>
+      <div className="Form">
+        <form>
+          <label>
+            Name
+            <input type="text" required />
+          </label>
+          <label>
+            Cooking Time
+            <input type="number" required />
+          </label>
+          <label>
+            Servings
+            <input type="number" min="1" required />
+          </label>
+          <label>
+            Instructions
+            <textarea cols="30" rows="10" required></textarea>
+          </label>
+          <div className="Btns">
+            <button className="btn primary">Update</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
