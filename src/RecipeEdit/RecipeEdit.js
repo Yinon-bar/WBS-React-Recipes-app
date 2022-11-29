c23118f6f1f62b2a2222ab8c3088ec0e773b8a5c
Yinon bar <yinonbar1988@gmail.com>
2022-11-29 12:10:53 +0200

    context working

--- src/RecipeEdit/RecipeEdit.js
+++ src/RecipeEdit/RecipeEdit.js
@@ -4,7 +4,10 @@ import { FaTimes } from "react-icons/fa";
 function RecipeEdit() {
   return (
     <div className="RecipeEdit">
-      <FaTimes />
+      <button className="btn danger">
+        <FaTimes />
+        <form className="Form"></form>
+      </button>
     </div>
   );
 }
